f20bb6c6b344dfc930099e01c3bc80ec1faf8ac8
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2026-02-27 19:10:39 -0500

    Add a score cell component

--- app/src/main/java/com/example/wordcut/MainActivity.kt
+++ app/src/main/java/com/example/wordcut/MainActivity.kt
@@ -5,15 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -46,14 +48,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WordCutGame(modifier: Modifier = Modifier) {
     val initialWord = "Metelas"
-    val initialWordUpper = initialWord.uppercase()
-    val initialLetters: List<Char?> = initialWordUpper.toList()
-    //val words;
+    val initialLetters: List<Char?> = initialWord.uppercase().toList()
 
     LetterGrid(
         letters = initialLetters,
         nbColumns = initialWord.length+1,
-        modifier = Modifier.fillMaxWidth()
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
     )
 }
 
@@ -67,32 +69,94 @@ fun LetterGrid(
         modifier = modifier,
         columns = GridCells.Fixed(nbColumns),
         verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        userScrollEnabled = false
     ) {
-        items(letters) { letter ->
-            LetterCell(letter = letter)
+        items(nbColumns) { index ->
+            if (index < letters.size) {
+                LetterCell(letter = letters[index])
+            } else {
+                ScoreCell(1, 2)
+            }
         }
-
     }
 }
 
 @Composable
 fun LetterCell(letter: Char?) {
+    val isEmpty = (letter == null)
+    val backgroundColor = if (isEmpty) Color(0xFFEFEFEF) else Color(0xFF2FB39A)
+    val textColor = if (isEmpty) Color.Transparent else Color(0xFF0B3B33)
+
     Box(
         modifier = Modifier
             .aspectRatio(1f)
             .background(
-                Color(0xFF2FB39A),
-                RoundedCornerShape(12.dp)
+                color = backgroundColor,
+                shape = RoundedCornerShape(12.dp)
             ),
         contentAlignment = Alignment.Center
     ) {
         Text(
-            text = letter.toString() ?: "",
+            text = letter?.toString() ?: "",
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
-            color = Color(0xFF0B3B33)
+            color = textColor
         )
+    }
+}
+
+@Composable
+fun ScoreCell(point1: Int = 0, point2: Int = 0) {
+    Box (
+        modifier = Modifier
+            .aspectRatio(1f)
+            .border(
+                width = 3.dp,
+                color = Color.Black,
+                shape = RoundedCornerShape(12.dp)
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (point1 > 0) {
+                Box (
+                    modifier = Modifier.weight(1f),
+                    contentAlignment = Alignment.Center
+                ){
+                    Text(
+                        text = point1.toString(),
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+
+            if (point1 > 0 && point2 > 0){
+                Box(
+                    modifier = Modifier
+                        .width(3.dp)
+                        .fillMaxSize()
+                        .background(Color.Black)
+                )
+            }
+
+            if (point2 > 0) {
+                Box (
+                    modifier = Modifier.weight(1f),
+                    contentAlignment = Alignment.Center
+                ){
+                    Text(
+                        text = point2.toString(),
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -102,11 +166,4 @@ fun WordCutGamePreview() {
     WordCutTheme {
         WordCutGame()
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun LetterCellPreview() {
-        val letter = 'M'
-        LetterCell(letter)
 }
