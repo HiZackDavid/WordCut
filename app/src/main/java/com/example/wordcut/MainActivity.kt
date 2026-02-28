f2bbcee333daad48cbc44b2c5ed629d868f75745
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2026-02-27 23:07:38 -0500

    Update layout and components
    Replaced LazyVerticalGrid layout with simple Row and Columns for better control of cells.
    WordCutGame now contains a mockup of expected behaviors of the game in terms of layout, components and colors.

--- app/src/main/java/com/example/wordcut/MainActivity.kt
+++ app/src/main/java/com/example/wordcut/MainActivity.kt
@@ -4,24 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,108 +49,223 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WordCutGame(modifier: Modifier = Modifier) {
-    val initialWord = "Metelas"
-    val initialLetters: List<Char?> = initialWord.uppercase().toList()
+    val initialWord = "Matelas"
+    val initialLetters: List<Char> = initialWord.uppercase().toList()
+    val nbLetters = initialLetters.size
 
-    LetterGrid(
-        letters = initialLetters,
-        nbColumns = initialWord.length+1,
+    Column(
         modifier = modifier
             .fillMaxWidth()
-            .padding(16.dp)
-    )
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        WordCutRow(
+            letters = initialLetters,
+            nbCells = nbLetters,
+            nbActiveCells = nbLetters,
+            hasCommited = true,
+            scoreIsDisabled = true
+        )
+        WordCutRow(
+            letters = "METAL".toList(),
+            nbCells = nbLetters,
+            nbActiveCells = nbLetters-2,
+            hasCommited = true,
+            point1 = 2,
+            point2 = 2
+        )
+        WordCutRow(
+            letters = "LAME".toList(),
+            nbCells = nbLetters,
+            nbActiveCells = nbLetters-3,
+            hasCommited = true,
+            point1 = 3,
+            point2 = 2
+        )
+        WordCutRow(
+            letters = "AME".toList(),
+            nbCells = nbLetters,
+            nbActiveCells = nbLetters-4,
+            hasCommited = true,
+            point1 = 3
+        )
+        WordCutRow(
+            letters = "M".toList(),
+            nbCells = nbLetters,
+            nbActiveCells = nbLetters-5,
+            hasCommited = false
+        )
+    }
 }
 
 @Composable
-fun LetterGrid(
-    letters: List<Char?>,
-    nbColumns: Int,
-    modifier: Modifier = Modifier
+fun WordCutRow(
+    letters: List<Char>,
+    nbCells: Int,
+    nbActiveCells: Int,
+    hasCommited: Boolean,
+    scoreIsDisabled: Boolean = false,
+    point1: Int = 0,
+    point2: Int = 0
 ) {
-    LazyVerticalGrid(
-        modifier = modifier,
-        columns = GridCells.Fixed(nbColumns),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        userScrollEnabled = false
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        items(nbColumns) { index ->
-            if (index < letters.size) {
-                LetterCell(letter = letters[index])
-            } else {
-                ScoreCell(1, 2)
+        val cellModifier = Modifier
+            .weight(1f)
+            .aspectRatio(1f)
+
+        for (i in 0 until nbCells) {
+            when {
+                i >= nbActiveCells ->
+                    DisabledCell(cellModifier)
+                hasCommited && i < letters.size ->
+                    CommitedCell(letters[i], cellModifier)
+                !hasCommited && i < letters.size ->
+                    WriteCell(letter = letters[i], modifier = cellModifier)
+                !hasCommited && i < nbActiveCells ->
+                    WriteCell(modifier = cellModifier)
+                else -> DisabledCell(cellModifier)
             }
+        }
+
+        if (scoreIsDisabled) {
+            DisabledScoreCell(cellModifier)
+        } else {
+            ScoreCell(
+                cellModifier,
+                point1 = point1,
+                point2 = point2
+            )
         }
     }
 }
 
 @Composable
-fun LetterCell(letter: Char?) {
-    val isEmpty = (letter == null)
-    val backgroundColor = if (isEmpty) Color(0xFFEFEFEF) else Color(0xFF2FB39A)
-    val textColor = if (isEmpty) Color.Transparent else Color(0xFF0B3B33)
-
+fun DisabledCell(modifier: Modifier = Modifier) {
     Box(
-        modifier = Modifier
-            .aspectRatio(1f)
+        modifier = modifier
+            .background(Color.Gray, RoundedCornerShape(12.dp)),
+        contentAlignment = Alignment.Center
+    ) {}
+}
+
+@Composable
+fun CommitedCell(letter: Char, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
             .background(
-                color = backgroundColor,
+                color = Color(0xFF2FB39A),
                 shape = RoundedCornerShape(12.dp)
             ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = letter.toString(),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF0B3B33)
+        )
+    }
+}
+
+@Composable
+fun WriteCell(modifier: Modifier = Modifier, letter: Char? = null) {
+    Box(
+        modifier = modifier
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .border(2.dp, Color(0xFF2FB39A), RoundedCornerShape(12.dp)),
         contentAlignment = Alignment.Center
     ) {
         Text(
             text = letter?.toString() ?: "",
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
-            color = textColor
+            color = Color(0xFF2FB39A)
         )
     }
 }
 
 @Composable
-fun ScoreCell(point1: Int = 0, point2: Int = 0) {
+fun DisabledScoreCell(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .background(Color.Gray, RoundedCornerShape(12.dp))
+            .border(3.dp, Color.Black, RoundedCornerShape(12.dp))
+    ) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawLine(
+                color = Color.Red,
+                start = Offset(0f, size.height),
+                end = Offset(size.width, 0f),
+                strokeWidth = 6f
+            )
+        }
+    }
+}
+
+@Composable
+fun ScoreCell(modifier: Modifier = Modifier, point1: Int = 0, point2: Int = 0){
+    val shape = RoundedCornerShape(12.dp)
+
+    val point1BgColor = Color(0xFFF2C14E)
+    val point1TextColor = Color(0xFF1B1B1B)
+    val point2BgColor = Color(0xFF2D6F89)
+    val noPointBgColor = Color(0xFFF5F5F5)
+
     Box (
-        modifier = Modifier
-            .aspectRatio(1f)
+        modifier = modifier
+            .clip(shape)
+            .background(
+                color = Color.White,
+                shape = shape
+            )
             .border(
-                width = 3.dp,
+                width = 2.dp,
                 color = Color.Black,
-                shape = RoundedCornerShape(12.dp)
+                shape = shape
             ),
         contentAlignment = Alignment.Center
     ) {
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            if (point1 > 0) {
-                Box (
-                    modifier = Modifier.weight(1f),
-                    contentAlignment = Alignment.Center
-                ){
+        Row( modifier = Modifier.fillMaxSize() ) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(
+                        if (point1 > 0) point1BgColor else noPointBgColor
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                if (point1 > 0) {
                     Text(
                         text = point1.toString(),
                         fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold
+                        fontWeight = FontWeight.Bold,
+                        color = point1TextColor
                     )
                 }
             }
 
-            if (point1 > 0 && point2 > 0){
-                Box(
-                    modifier = Modifier
-                        .width(3.dp)
-                        .fillMaxSize()
-                        .background(Color.Black)
-                )
-            }
+            Box(
+                modifier = Modifier
+                    .width(1.dp)
+                    .fillMaxSize()
+                    .background(Color.Black)
+            )
 
             if (point2 > 0) {
-                Box (
-                    modifier = Modifier.weight(1f),
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                        .background(point2BgColor),
                     contentAlignment = Alignment.Center
-                ){
+                ) {
                     Text(
                         text = point2.toString(),
                         fontSize = 18.sp,
