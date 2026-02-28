6c0f260a138130ce6297154276b14a7dc8bbf06c
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2026-02-28 15:35:50 -0500

    Create data class for the game screen and the rows

--- app/src/main/java/com/example/wordcut/ui/components/WordCutRow.kt
+++ app/src/main/java/com/example/wordcut/ui/components/WordCutRow.kt
@@ -14,110 +14,112 @@ import com.example.wordcut.ui.components.cells.DisabledScoreCell
 import com.example.wordcut.ui.components.cells.ScoreCell
 import com.example.wordcut.ui.components.cells.WriteCell
 
+data class WordCutRowState(
+    val letters: List<Char>,
+    val nbCells: Int,
+    val nbActiveCells: Int,
+    val hasCommitted: Boolean = false,
+    val isScoreDisabled: Boolean = false,
+    val point1: Int = 0,
+    val point2: Int = 0
+)
+
 @Composable
-fun WordCutRow(
-    letters: List<Char>,
-    nbCells: Int,
-    nbActiveCells: Int,
-    hasCommitted: Boolean,
-    isScoreDisabled: Boolean = false,
-    point1: Int = 0,
-    point2: Int = 0
-) {
+fun WordCutRow(row: WordCutRowState, modifier: Modifier = Modifier) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         val cellModifier = Modifier
             .weight(1f)
             .aspectRatio(1f)
 
-        for (i in 0 until nbCells) {
+        for (i in 0 until row.nbCells) {
             when {
-                i >= nbActiveCells ->
+                i >= row.nbActiveCells ->
                     DisabledCell(cellModifier)
-                hasCommitted && i < letters.size ->
-                    CommittedCell(letters[i], cellModifier)
-                !hasCommitted && i < letters.size ->
-                    WriteCell(letter = letters[i], modifier = cellModifier)
-                !hasCommitted && i < nbActiveCells ->
+                row.hasCommitted && i < row.letters.size ->
+                    CommittedCell(row.letters[i], cellModifier)
+                !row.hasCommitted && i < row.letters.size ->
+                    WriteCell(letter = row.letters[i], modifier = cellModifier)
+                !row.hasCommitted && i < row.nbActiveCells ->
                     WriteCell(modifier = cellModifier)
                 else -> DisabledCell(cellModifier)
             }
         }
 
-        if (isScoreDisabled) {
+        if (row.isScoreDisabled) {
             DisabledScoreCell(cellModifier)
         } else {
             ScoreCell(
                 cellModifier,
-                point1 = point1,
-                point2 = point2
+                point1 = row.point1,
+                point2 = row.point2
             )
         }
     }
 }
 
+private fun getDemoData(): Map<String, WordCutRowState>{
+    val word = "MATELAS"
+    val letters = word.uppercase().toList()
+
+    return mapOf(
+        "First Row" to WordCutRowState(
+            letters = letters,
+            nbCells = letters.size,
+            nbActiveCells = letters.size,
+            hasCommitted = true,
+            isScoreDisabled = true
+        ),
+        "Dual Score" to WordCutRowState(
+            letters = "METAL".toList(),
+            nbCells = letters.size,
+            nbActiveCells = letters.size-2,
+            hasCommitted = true,
+            point1 = 2,
+            point2 = 2
+        ),
+        "Single Score" to WordCutRowState(
+            letters = "AME".toList(),
+            nbCells = letters.size,
+            nbActiveCells = letters.size-4,
+            hasCommitted = true,
+            point1 = 3
+        ),
+        "Playable" to WordCutRowState(
+            letters = "M".toList(),
+            nbCells = letters.size,
+            nbActiveCells = letters.size-5,
+            hasCommitted = false
+        )
+    )
+}
+
 @Preview
 @Composable
 fun FirstRowPreview() {
-    val initialWord = "Matelas"
-    val initialLetters: List<Char> = initialWord.uppercase().toList()
-    val nbLetters = initialLetters.size
-
-    WordCutRow(
-        letters = initialLetters,
-        nbCells = nbLetters,
-        nbActiveCells = nbLetters,
-        hasCommitted = true,
-        isScoreDisabled = true
-    )
+    val row = getDemoData()["First Row"]
+    if (row != null) WordCutRow(row)
 }
 
 @Preview
 @Composable
 fun DualScorePreview() {
-    val initialWord = "Matelas"
-    val initialLetters: List<Char> = initialWord.uppercase().toList()
-    val nbLetters = initialLetters.size
-
-    WordCutRow(
-        letters = "METAL".toList(),
-        nbCells = nbLetters,
-        nbActiveCells = nbLetters-2,
-        hasCommitted = true,
-        point1 = 2,
-        point2 = 2
-    )
+    val row = getDemoData()["Dual Score"]
+    if (row != null) WordCutRow(row)
 }
 
 @Preview
 @Composable
 fun SingleScorePreview() {
-    val initialWord = "Matelas"
-    val initialLetters: List<Char> = initialWord.uppercase().toList()
-    val nbLetters = initialLetters.size
-
-    WordCutRow(
-        letters = "AME".toList(),
-        nbCells = nbLetters,
-        nbActiveCells = nbLetters-4,
-        hasCommitted = true,
-        point1 = 3
-    )
+    val row = getDemoData()["Single Score"]
+    if (row != null) WordCutRow(row)
 }
 
 @Preview
 @Composable
 fun PlayablePreview() {
-    val initialWord = "Matelas"
-    val initialLetters: List<Char> = initialWord.uppercase().toList()
-    val nbLetters = initialLetters.size
-
-    WordCutRow(
-        letters = "M".toList(),
-        nbCells = nbLetters,
-        nbActiveCells = nbLetters-5,
-        hasCommitted = false
-    )
+    val row = getDemoData()["Playable"]
+    if (row != null) WordCutRow(row)
 }
--- app/src/main/java/com/example/wordcut/ui/screens/GameScreen.kt
+++ app/src/main/java/com/example/wordcut/ui/screens/GameScreen.kt
@@ -9,13 +9,51 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.wordcut.ui.components.WordCutRow
+import com.example.wordcut.ui.components.WordCutRowState
 import com.example.wordcut.ui.theme.WordCutTheme
 
 @Composable
-fun GameScreen(modifier: Modifier = Modifier) {
-    val initialWord = "Matelas"
-    val initialLetters: List<Char> = initialWord.uppercase().toList()
-    val nbLetters = initialLetters.size
+fun GameScreen(word: String, modifier: Modifier = Modifier) {
+    val letters: List<Char> = word.uppercase().toList()
+    val nbLetters = letters.size
+    val words = listOf(
+        WordCutRowState(
+            letters = letters,
+            nbCells = nbLetters,
+            nbActiveCells = nbLetters,
+            hasCommitted = true,
+            isScoreDisabled = true
+        ),
+        WordCutRowState(
+            letters = "METAL".toList(),
+            nbCells = nbLetters,
+            nbActiveCells = nbLetters-2,
+            hasCommitted = true,
+            point1 = 2,
+            point2 = 2
+        ),
+        WordCutRowState(
+            letters = "LAME".toList(),
+            nbCells = nbLetters,
+            nbActiveCells = nbLetters-3,
+            hasCommitted = true,
+            point1 = 3,
+            point2 = 2
+        ),
+        WordCutRowState(
+            letters = "AME".toList(),
+            nbCells = nbLetters,
+            nbActiveCells = nbLetters-4,
+            hasCommitted = true,
+            point1 = 3
+        ),
+        WordCutRowState(
+            letters = "M".toList(),
+            nbCells = nbLetters,
+            nbActiveCells = nbLetters-5,
+            hasCommitted = false
+        )
+    )
 
     Column(
         modifier = modifier
@@ -23,42 +61,9 @@ fun GameScreen(modifier: Modifier = Modifier) {
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ){
-        WordCutRow(
-            letters = initialLetters,
-            nbCells = nbLetters,
-            nbActiveCells = nbLetters,
-            hasCommitted = true,
-            isScoreDisabled = true
-        )
-        WordCutRow(
-            letters = "METAL".toList(),
-            nbCells = nbLetters,
-            nbActiveCells = nbLetters-2,
-            hasCommitted = true,
-            point1 = 2,
-            point2 = 2
-        )
-        WordCutRow(
-            letters = "LAME".toList(),
-            nbCells = nbLetters,
-            nbActiveCells = nbLetters-3,
-            hasCommitted = true,
-            point1 = 3,
-            point2 = 2
-        )
-        WordCutRow(
-            letters = "AME".toList(),
-            nbCells = nbLetters,
-            nbActiveCells = nbLetters-4,
-            hasCommitted = true,
-            point1 = 3
-        )
-        WordCutRow(
-            letters = "M".toList(),
-            nbCells = nbLetters,
-            nbActiveCells = nbLetters-5,
-            hasCommitted = false
-        )
+        for (word in words) {
+            WordCutRow(word)
+        }
     }
 }
 
@@ -66,6 +71,6 @@ fun GameScreen(modifier: Modifier = Modifier) {
 @Composable
 fun GameGamePreview() {
     WordCutTheme {
-        GameScreen()
+        GameScreen(word = "Matelas")
     }
 }
